63498c62bd246acbf37135e5f344fdf8253fb581
John <john@kpmpower.com>
2021-01-14 14:00:58 -0500

    fixed formatting issue

--- src/Hook.js
+++ src/Hook.js
@@ -18,10 +18,12 @@ const Hook = () => {
       {fileOptions.map(option=><option key={`o-${option.val}`} value={option.val}>{option.text}</option>)}
     </select>
     {content.length > 0 && <p>Showing <strong>{content.length}</strong> records</p>}
-    <div id="display">
     <table>
     <thead>
       <tr>
+	<td>Name</td>
+	<td>Age</td>
+	<td>Phone</td>
 	<td>Name</td>
 	<td>Age</td>
 	<td>Phone</td>
@@ -33,29 +35,13 @@ const Hook = () => {
         <td><input type="number" value={row.name} onChange={e => editRecord(k,'name',e.currentTarget.value)} /></td>
         <td><input type="number" value={row.age} onChange={e => editRecord(k,'age',e.currentTarget.value)} /></td>
         <td><input type="number" value={row.phone} onChange={e => editRecord(k,'phone',e.currentTarget.value)} /></td>
-      </tr>
-    )) : <tr><td colSpan="3">No records available</td></tr>}
-    </tbody>
-    </table>
-    <table>
-    <thead>
-      <tr>
-	<td>Name</td>
-	<td>Age</td>
-	<td>Phone</td>
-      </tr>
-    </thead>
-    <tbody>
-    {content.map((row,k)=>(
-      <tr key={`read-${k}`}>
         <td>{row.name}</td>
         <td>{row.age}</td>
         <td>{row.phone}</td>
       </tr>
-    ))}
+    )) : <tr><td colSpan="3">No records available</td></tr>}
     </tbody>
     </table>
-    </div>
     </div>
   );
 };
